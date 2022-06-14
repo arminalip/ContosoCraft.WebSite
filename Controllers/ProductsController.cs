163475ba38c4d1bc27622c14a2269565b7aef36b
arminalip <arminalip@gmail.com>
2022-06-14 16:37:38 +0430

    Initial check-in of module ContosoCrafts

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
